c0ceb768aaadc80ba29617b9bb2285056fee23f9
Hunter Brodie <hunterbrodie@gmail.com>
2022-05-02 22:21:49 -0400

    test'

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,53 @@
 extern crate image;
-use image::{ImageBuffer};
+use image::ImageBuffer;
 
 use std::env;
 
 use std::thread;
 
-fn main()
+fn main() -> Result<(), Box<dyn std::error::Error>>
 {
 	let args: Vec<String> = env::args().collect();
-	let sections = args[1].parse::<i32>().unwrap();
+	let width_steps = args[1].parse::<u32>()?;
 
-	let step: f64 = 2.1 / (sections as f64);
+	let data = calculate_set(width_steps);
+
+	write_to_image(data);
+
+	Ok(())
+}
+
+fn write_to_image(data: Vec<Vec<u8>>)
+{
+	let img = ImageBuffer::from_fn(data.len() as u32, data[0].len() as u32, |x, y|
+	{
+		let color = data[x as usize][y as usize];
+		if color == 32
+		{
+			image::Rgb([0u8, 0u8, 0u8])
+		}
+		else
+		{
+			image::Rgb([(data[x as usize][y as usize] as f32 / 32f32 * 255f32) as u8, 127u8, 127u8])
+		}
+	});
+
+	img.save("mandelbrot.png").unwrap();
+}
+
+fn calculate_set(width_steps: u32) -> Vec<Vec<u8>>
+{
+	let increment: f32 = 2.7 / (width_steps as f32);
+	let height_steps: u32 = (2.4 / increment).ceil() as u32;
 	let mut data: Vec<Vec<u8>> = Vec::new();
 
 	let mut handles: Vec<std::thread::JoinHandle<Vec<u8>>> = Vec::new();
 
 	let mut threads = 0;
 
-	for z in -sections..sections
+	for z in 0..width_steps
 	{
-		let j = f64::from(z) * step;
+		let j = z as f32 * increment - 2.1;
 
 		threads += 1;
 
@@ -37,9 +65,9 @@ fn main()
 		{
 			let mut row: Vec<u8> = Vec::new();
 
-			for i in -sections..sections
+			for i in 0..height_steps
 			{
-				let k = f64::from(i) * step;
+				let k = i as f32 * increment - 1.2;
 	
 				row.push(mandelbrot(0.0, 0.0, j, k, 0));
 			}
@@ -53,30 +81,16 @@ fn main()
 		data.push(handle.join().unwrap());
 	}
 
-	println!("Done Calculating Mandelbrot Set!");
-
-	write_to_image(data);
-
-	println!("Done Writing Mandelbrot Set!");
+	data
 }
 
-fn write_to_image(data: Vec<Vec<u8>>)
+fn mandelbrot(z: f32, i: f32, z0: f32, i0: f32, n: u8) -> u8
 {
-	let img = ImageBuffer::from_fn(data.len() as u32, data[0].len() as u32, |x, y|
-	{
-		image::Luma([data[x as usize][y as usize]])
-	});
-
-	img.save("mandelbrot.png").unwrap();
-}
-
-fn mandelbrot(z: f64, i: f64, z0: f64, i0: f64, n: u8) -> u8
-{
-	let i_n: f64 = 2.0 * z * i + &i0;
-	let z_n: f64 = z.powi(2) - i.powi(2) + &z0;
+	let i_n: f32 = 2.0 * z * i + &i0;
+	let z_n: f32 = z.powi(2) - i.powi(2) + &z0;
 	if bounded(z_n, i_n)
 	{
-		if n == 255
+		if n == 32
 		{
 			return n
 		}
@@ -91,9 +105,9 @@ fn mandelbrot(z: f64, i: f64, z0: f64, i0: f64, n: u8) -> u8
 	}
 }
 
-fn bounded(z: f64, i: f64) -> bool
+fn bounded(z: f32, i: f32) -> bool
 {
-	let mut dist: f64 = z.powi(2) + i.powi(2);
+	let mut dist: f32 = z.powi(2) + i.powi(2);
 	dist = dist.sqrt();
 	if dist < 16.0
 	{
